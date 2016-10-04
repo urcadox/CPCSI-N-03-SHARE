731e9d97a54cb586852930186506f909c23c11ed
simonL <simon.louvet.zen@gmail.com>
2016-10-04 16:18:08 +0200

    fin correction Junit

--- tennisRule/service/src/main/java/org/imie/Main.java
+++ tennisRule/service/src/main/java/org/imie/Main.java
@@ -1,7 +1,31 @@
 package org.imie;
+
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("bienvenue au decompte de score de tennis");
+		
+		Jeux jeux = new Jeux();
+		
+		Scanner scanner= new Scanner(System.in);
+		
+		Boolean finAppli = false;
+		while (!finAppli) {
+			System.out.println("quel joueur marque");
+			String saisie = scanner.next();
+			if (saisie.compareTo("X")==0){
+				finAppli=true;
+			}else {
+				if(saisie.compareTo("1")==0){
+					jeux.marquerJoueur1();
+				}else if (saisie.compareTo("2")==0) {
+					jeux.marquerJoueur2();
+				}
+			}
+			
+			System.out.format("score :%s \n", jeux.getScore());
+		}
 	}
 
 	/* (non-Java-doc)
